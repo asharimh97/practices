11e630496c072ced707b8b520b88593cf81df909
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2019-06-27 10:57:04 +0700

    Add ANT design practices

---
+++ antd-practices/src/pages/.umi/router.js
@@ -0,0 +1,88 @@
+import React from 'react';
+import { Router as DefaultRouter, Route, Switch } from 'react-router-dom';
+import dynamic from 'umi/dynamic';
+import renderRoutes from 'umi/lib/renderRoutes';
+import history from '@tmp/history';
+import RendererWrapper0 from '/Volumes/Data Center/practices/antd-practices/src/pages/.umi/LocaleWrapper.jsx'
+import _dvaDynamic from 'dva/dynamic'
+
+const Router = require('dva/router').routerRedux.ConnectedRouter;
+
+const routes = [
+  {
+    "path": "/",
+    "component": __IS_BROWSER
+    ? _dvaDynamic({
+      
+      component: () => import(/* webpackChunkName: "layouts__BasicLayout" */'../../layouts/BasicLayout'),
+      LoadingComponent: require('/Volumes/Data Center/practices/antd-practices/src/components/PageLoading/index').default,
+    })
+    : require('../../layouts/BasicLayout').default,
+    "Routes": [require('../Authorized').default],
+    "authority": [
+      "admin",
+      "user"
+    ],
+    "routes": [
+      {
+        "path": "/",
+        "name": "welcome",
+        "icon": "smile",
+        "component": __IS_BROWSER
+    ? _dvaDynamic({
+      
+      component: () => import(/* webpackChunkName: "p__Welcome" */'../Welcome'),
+      LoadingComponent: require('/Volumes/Data Center/practices/antd-practices/src/components/PageLoading/index').default,
+    })
+    : require('../Welcome').default,
+        "exact": true
+      },
+      {
+        "path": "/lorem",
+        "name": "lorem",
+        "component": __IS_BROWSER
+    ? _dvaDynamic({
+      
+      component: () => import(/* webpackChunkName: "p__Lorem" */'../Lorem'),
+      LoadingComponent: require('/Volumes/Data Center/practices/antd-practices/src/components/PageLoading/index').default,
+    })
+    : require('../Lorem').default,
+        "exact": true
+      },
+      {
+        "component": () => React.createElement(require('/Volumes/Data Center/practices/antd-practices/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+      }
+    ]
+  },
+  {
+    "component": () => React.createElement(require('/Volumes/Data Center/practices/antd-practices/node_modules/umi-build-dev/lib/plugins/404/NotFound.js').default, { pagesPath: 'src/pages', hasRoutesInConfig: true })
+  }
+];
+window.g_routes = routes;
+const plugins = require('umi/_runtimePlugin');
+plugins.applyForEach('patchRoutes', { initialValue: routes });
+
+// route change handler
+function routeChangeHandler(location, action) {
+  plugins.applyForEach('onRouteChange', {
+    initialValue: {
+      routes,
+      location,
+      action,
+    },
+  });
+}
+history.listen(routeChangeHandler);
+routeChangeHandler(history.location);
+
+export { routes };
+
+export default function RouterWrapper(props = {}) {
+  return (
+<RendererWrapper0>
+          <Router history={history}>
+      { renderRoutes(routes, props) }
+    </Router>
+        </RendererWrapper0>
+  );
+}
---
+++ antd-practices/src/pages/Lorem.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Lorem extends Component {
+  render() {
+    return (
+      <div>
+        <p>Ini halaman lorem </p>
+      </div>
+    );
+  }
+}
+
+export default Lorem;

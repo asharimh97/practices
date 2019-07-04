06bf1ed783f679abfc6c4703d5025b1173616799
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2019-07-04 12:17:22 +0700

    Add congrats component

---
+++ react-practices/guess-word/src/components/Congrats.js
@@ -0,0 +1,31 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+/**
+ * Functional react component for congaratulatory message.
+ * @function
+ * @param {object} props = React props.
+ * @returns {JSX.Element} - Rendered component.
+*/
+
+const Congrats = (props) => {
+  if (props.success) {
+    return (
+      <div data-test="component-congrats">
+        <p data-test="congrats-message">
+          Congratulations! You guessed the word!
+        </p>
+      </div>
+    )
+  } else {
+    return (
+      <div data-test="component-congrats"></div>
+    )
+  }
+}
+
+Congrats.propTypes = {
+  success: PropTypes.bool.isRequired
+};
+
+export default Congrats ;

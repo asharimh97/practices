6255e95e4e032f30bc19be7765c5918c880e36b9
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2019-07-08 13:08:53 +0700

    Test and code for non-zero guessed words

--- react-practices/guess-word/src/components/GuessedWords.js
+++ react-practices/guess-word/src/components/GuessedWords.js
@@ -4,8 +4,28 @@ import PropTypes from "prop-types";
 const GuessedWords = props => {
   return (
     <div data-test="component-guessed-words">
-      {props.guessedWords.length === 0 && (
+      {props.guessedWords.length === 0 ? (
         <p data-test="component-instruction">Try to guess the secret word</p>
+      ) : (
+        <div data-test="guessed-words">
+          <h4>Guessed words</h4>
+          <table>
+            <thead>
+              <tr>
+                <th>Guess</th>
+                <th>Matching letters</th>
+              </tr>
+            </thead>
+            <tbody>
+              {props.guessedWords.map((word, idx) => (
+                <tr key={idx} data-test="guessed-word">
+                  <td>{word.guessedWord}</td>
+                  <td>{word.letterMatchCount}</td>
+                </tr>
+              ))}
+            </tbody>
+          </table>
+        </div>
       )}
     </div>
   )

f657c908af87eaaec3f286b62e826d0f630d8a94
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2019-07-15 15:20:33 +0700

    Add random word server for guess word project

---
+++ react-practices/random-word-server/server.js
@@ -0,0 +1,25 @@
+const express = require("express");
+const fs = require("fs");
+
+const app = express();
+
+// read words from json file
+const fileContents = fs.readFileSync("./five-letter-words.json", "utf-8");
+const words = JSON.parse(fileContents);
+
+const { fiveLetterWords } = words;
+
+app.get("/", (req, res) => {
+  // select a random word
+  const word = fiveLetterWords[Math.floor(Math.random() * fiveLetterWords.length)];
+
+  // return response
+  res.send(word);
+});
+
+app.listen(3030, () => {
+  console.log("Word sever listenin on port 3030");
+});
+
+module.exports = app;
+

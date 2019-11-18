fb1c00c8c0facaa5941e5996f08ff272c15334ed
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2019-11-18 11:56:33 +0700

    Update App component

--- react-practices/jotto-hooks/src/App.test.js
+++ react-practices/jotto-hooks/src/App.test.js
@@ -1,9 +1,14 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import App from './App';
+import React from "react";
+import { shallow } from "enzyme";
+import App from "./App";
+import { findComponent } from "../test/testUtils";
 
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
+const setup = (props = {}) => {
+  const wrapper = shallow(<App />);
+  return wrapper;
+};
+
+test("renders without error", () => {
+  const wrapper = setup();
+  findComponent(wrapper, "component-app");
 });

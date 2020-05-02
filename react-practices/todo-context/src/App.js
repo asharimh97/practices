26cc6b8a1eab3b053ba1fd5970edadd15cc30263
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-05-02 22:04:46 +0700

    Update app component

--- react-practices/todo-context/src/App.js
+++ react-practices/todo-context/src/App.js
@@ -1,25 +1,65 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useRef } from 'react';
 import './App.css';
+import InputTodo from './components/InputTodo';
+import ListTodos from './components/ListTodos';
+import TodoContext from './context/TodoContext';
+import ButtonTab from './components/ButtonTab';
 
 function App() {
+  /**
+   * Active tabs contains of "all", "completed", and "deleted"
+   */
+  const [activeTab, setActiveTab] = useState("all");
+  const [todos, setTodos] = useState([]);
+  const inputRef = useRef(null);
+
+  const contextItem = {
+    todos,
+    setTodos
+  };
+
+  const generateId = () => {
+    return '_' + Math.random().toString(36).substr(2, 9);
+  }
+
+  const handleAddTodo = () => {
+    const newTodo = {
+      id: generateId(),
+      todo: inputRef.current.value,
+      status: "added"
+    };
+
+    setTodos([].concat(todos, newTodo));
+
+    inputRef.current.value = "";
+  };
+  
+  const completedTodos = todos.filter(td => td.status === "completed");
+  const deletedTodos = todos.filter(td => td.status === "deleted");
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <TodoContext.Provider value={contextItem}>
+      <div className="container">
+        <h1>To Do ...</h1>
+        <InputTodo 
+          data-test="input-todo" 
+          ref={inputRef} 
+          placeholder="Input to do here" 
+          onSubmit={handleAddTodo} 
+        />
+        <ButtonTab background="#e9faf1" color="#000" onClick={() => setActiveTab("all")}>
+          All ({todos.length})
+        </ButtonTab>
+        <ButtonTab background="#22c971" color="#fff" onClick={() => setActiveTab("completed")}>
+          Completed ({completedTodos.length})
+        </ButtonTab>
+        <ButtonTab background="#fd727d" color="#fd4f5d" onClick={() => setActiveTab("deleted")}>
+          Deleted ({deletedTodos.length})
+        </ButtonTab>
+        
+        <ListTodos display={activeTab} />
+      </div>
+    </TodoContext.Provider>
   );
 }
 

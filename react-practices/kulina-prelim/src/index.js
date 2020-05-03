454c9d6425b6126f1abc137cdb20c8e000c45592
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-05-04 05:57:20 +0700

    Wrap app in theme provider

--- react-practices/kulina-prelim/src/index.js
+++ react-practices/kulina-prelim/src/index.js
@@ -3,11 +3,15 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
+import { ThemeProvider } from 'styled-components';
+import theme from './utils/theme';
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </ThemeProvider>,
   document.getElementById('root')
 );
 

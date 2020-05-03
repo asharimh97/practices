1810423f08bcec9367fc60686c52868c01c3d5b4
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-05-03 21:06:48 +0700

    Add color theme

---
+++ react-practices/kulina-prelim/src/utils/theme.js
@@ -0,0 +1,13 @@
+const colors = {
+  red: "#f9234a",
+  darkRed: "#a23530",
+  charcoal: "#424749",
+  metalGray: "#6e7679",
+  white: "#f1f1f2",
+  shadedWhite: "#e2e4e4"
+};
+
+export default {
+  colors
+};
+

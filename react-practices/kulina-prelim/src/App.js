e7baed0646dd557ef3c76ba90da703e9cad3e24f
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-05-04 05:57:09 +0700

    Update app components

--- react-practices/kulina-prelim/src/App.js
+++ react-practices/kulina-prelim/src/App.js
@@ -1,25 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
+import React, { useState, useMemo } from 'react';
 import './App.css';
+import AddressBar from './components/AddressBar';
+import { generateStartDay } from './utils/helper';
+import DateList from './components/DateList';
+import AppContext from './context/AppContext';
+import ButtonTimeslot from './components/ButtonTimeslot';
+import ListFood from './components/ListFood';
+import Cart from './components/Cart';
+import ModalLocation from './components/ModalLocation';
 
 function App() {
+  const startDate = generateStartDay();
+  const appState = {
+    activeDate: startDate,
+    timeslot: "lunch",
+    cart: []
+  };
+  const [state, setState] = useState(appState);
+  const [address, setAddress] = useState("Tokopedia Tower");
+  const [showModal, setShowModal] = useState(false);
+
+  const appContextValue = useMemo(() => ({
+    state,
+    setState
+  }), [state, setState]);
+
+  const handleSetAddress = addr => {
+    setAddress(addr);
+    setShowModal(false);
+  };
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppContext.Provider value={appContextValue}>
+      <div className="container">
+        <AddressBar 
+          onShowAddressModal={() => setShowModal(true)} 
+          currentAddress={address} 
+        />
+        <DateList startDate={startDate} activeDate={state.activeDate} />
+
+        <ButtonTimeslot />
+        <ListFood />
+        <Cart />
+
+        <ModalLocation
+          visible={showModal}
+          onCloseModal={() => setShowModal(false)} 
+          onSelectAddress={handleSetAddress}
+        />
+      </div>
+    </AppContext.Provider>
   );
 }
 

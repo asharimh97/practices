c811b485140f94e4a241bd7fe6bfd4acd3d260b6
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-05-16 14:23:11 +0700

    Add config to format markdown to page

--- react-practices/markdown-page/gatsby-node.js
+++ react-practices/markdown-page/gatsby-node.js
@@ -4,4 +4,39 @@
  * See: https://www.gatsbyjs.org/docs/node-apis/
  */
 
-// You can delete this file if you're not using it
+const path = require("path");
+
+exports.createPages = async ({ actions, graphql, reporter }) => {
+  const { createPage } = actions;
+
+  const blogTemplate = path.resolve(`src/pages/templates/BlogTemplate.js`);
+
+  const res = await graphql(`
+    {
+      allMarkdownRemark(
+        sort: { order: DESC, fields: [frontmatter___date] }
+        limit: 1000
+      ) {
+        edges {
+          node {
+            frontmatter {
+              path
+            }
+          }
+        }
+      }
+    }
+  `);
+
+  if (res.errors) {
+    reporter.panicOnBuild(`Error while running GraphQL query`);
+    return
+  }
+
+  res.data.allMarkdownRemark.edges.forEach(({ node }) => {
+    createPage({
+      path: node.frontmatter.path,
+      component: blogTemplate
+    });
+  });
+};

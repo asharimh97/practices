f2373075b95205ae2b744b62809a3c8cd0182fb5
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-11-19 21:00:16 +0700

    Clear layout component

--- react-practices/kanban-typescript/src/components/Layout.tsx
+++ react-practices/kanban-typescript/src/components/Layout.tsx
@@ -1,6 +1,5 @@
 import React from "react";
 import { Container } from "./atoms";
-import Navbar from "./molecules/Navbar";
 import SEO from "./SEO";
 
 type Prop = {
@@ -12,7 +11,6 @@ const Layout: React.FC<Prop> = ({ children, title, ...props }) => {
   return (
     <>
       {title && <SEO title={title} />}
-      <Navbar />
       <Container
         height={["100vh", null, "calc(100vh - 98px)"]}
         maxHeight="100%"

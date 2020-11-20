b846de48818dbada79132a5933d56187de8dd4ce
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-11-20 19:00:05 +0700

    Update list task to be a droppable component

--- react-practices/kanban-typescript/src/components/organisms/ListTask.tsx
+++ react-practices/kanban-typescript/src/components/organisms/ListTask.tsx
@@ -1,18 +1,55 @@
 import { Box, Title } from "components/atoms";
 import CardItem from "components/molecules/CardItem";
 import React from "react";
+import { Droppable } from "react-beautiful-dnd";
 
-type Props = {};
+type Props = {
+  title?: string;
+  tasks?: Array<any>;
+  [key: string]: any;
+};
 
-const ListTask: React.FC<Props> = props => {
+const ListTask: React.FC<Props> = ({
+  children,
+  tasks,
+  id,
+  title,
+  ...props
+}) => {
   return (
-    <Box>
-      <Title as="h5">Task Title</Title>
-      {[...new Array(10)].map((item, idx) => (
-        <CardItem key={idx} />
-      ))}
+    <Box {...props}>
+      <Title as="h5">{title}</Title>
+      <Droppable droppableId={id}>
+        {provided => (
+          <Box
+            bg="rgba(0,0,0,0.3)"
+            borderRadius="8px"
+            p={3}
+            ref={provided.innerRef}
+            {...provided.droppableProps}
+          >
+            {tasks?.map((item, idx) => {
+              return (
+                <CardItem
+                  task={item}
+                  id={`${id}-task-${idx}`}
+                  key={idx}
+                  index={idx}
+                />
+              );
+            })}
+            {provided.placeholder}
+          </Box>
+        )}
+      </Droppable>
     </Box>
   );
+};
+
+ListTask.defaultProps = {
+  tasks: [...new Array(5)],
+  id: "droppable-task",
+  title: "Title Tasks"
 };
 
 export default ListTask;

61b9d7ec3995997d48cc150f9fe1498449e7005e
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-11-21 20:29:01 +0700

    Apply move task from same list

--- react-practices/kanban-typescript/src/App.tsx
+++ react-practices/kanban-typescript/src/App.tsx
@@ -1,14 +1,39 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Container, Grid } from "components/atoms";
 import { DragDropContext } from "react-beautiful-dnd";
 import ListTask from "components/organisms/ListTask";
 import tasks from "__mocks__/tasks";
 
 function App() {
-  const [taskList, setTaskList] = useState(tasks);
+  const taskListIds = [
+    "task-droppable",
+    "task-droppable-2",
+    "task-droppable-3"
+  ];
 
-  const reorder = (sourceIndex: number, destinationIndex: number) => {
-    const tasks = [...taskList];
+  const mapTaskList = taskListIds.reduce(
+    (mapTasks: Record<string, any>, current) => {
+      mapTasks[current] = tasks;
+
+      return mapTasks;
+    },
+    {}
+  );
+
+  const [taskList, setTaskList] = useState<Record<string, any>>({});
+
+  useEffect(() => {
+    setTaskList(mapTaskList);
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, []);
+
+  // Only applies to same list
+  const reorder = (
+    list: Array<any>,
+    sourceIndex: number,
+    destinationIndex: number
+  ) => {
+    const tasks = [...list];
     const [removed] = tasks.splice(sourceIndex, 1);
 
     tasks.splice(destinationIndex, 0, removed);
@@ -16,26 +41,43 @@ function App() {
     return tasks;
   };
 
+  const getList = (id: string) => taskList[id];
+
   const handleDragEnd = (e: any) => {
     const { source, destination } = e;
 
-    const newTaskList = reorder(source.index, destination.index);
+    if (source.droppableId === destination.droppableId) {
+      const newTaskList = reorder(
+        taskList[source.droppableId],
+        source.index,
+        destination.index
+      );
 
-    setTaskList(newTaskList);
+      setTaskList({
+        ...taskList,
+        [source.droppableId]: newTaskList
+      });
+    } else {
+      console.log("Kamu mindah data ya hmm");
+      console.log(
+        getList(source.droppableId),
+        getList(destination.droppableId)
+      );
+    }
   };
 
   return (
     <Container>
       <DragDropContext onDragEnd={handleDragEnd}>
         <Grid columns="repeat(4, minmax(180px, 250px))" gap={3}>
-          <ListTask />
-          <ListTask
-            title="Task List 2"
-            id="task-droppable-2"
-            tasks={taskList}
-          />
-          <ListTask title="Task List 4" id="task-droppable-3" />
-          <ListTask title="Task List 3" id="task-droppable-4" />
+          {taskListIds.map((listId, idx) => (
+            <ListTask
+              key={idx}
+              id={listId}
+              title={`Task List #${idx}`}
+              tasks={taskList[listId]}
+            />
+          ))}
         </Grid>
       </DragDropContext>
     </Container>

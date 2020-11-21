dcd8c803d2c7b3e69653d81de42b2d5704fecfb1
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-11-21 20:02:41 +0700

    Add tasks list mock data

---
+++ react-practices/kanban-typescript/src/__mocks__/tasks.ts
@@ -0,0 +1,24 @@
+const tasks: Array<Record<string, any>> = [
+  {
+    task: "Makan Bakso",
+    dueDate: "2020-10-11"
+  },
+  {
+    task: "Makan Mie Ayam",
+    dueDate: "2020-10-12"
+  },
+  {
+    task: "Ngemil malam",
+    dueDate: "2020-11-11"
+  },
+  {
+    task: "Apa ya task nya",
+    dueDate: "2020-09-11"
+  },
+  {
+    task: "Task yang lain",
+    dueDate: "2020-10-11"
+  }
+];
+
+export default tasks;

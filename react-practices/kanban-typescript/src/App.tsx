f67a599f7cec7abf996e1ab336d045214f11934b
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-11-21 20:03:03 +0700

    Implement reordering tasks

--- react-practices/kanban-typescript/src/App.tsx
+++ react-practices/kanban-typescript/src/App.tsx
@@ -1,12 +1,27 @@
-import React from "react";
+import React, { useState } from "react";
 import { Container, Grid } from "components/atoms";
 import { DragDropContext } from "react-beautiful-dnd";
 import ListTask from "components/organisms/ListTask";
+import tasks from "__mocks__/tasks";
 
 function App() {
+  const [taskList, setTaskList] = useState(tasks);
+
+  const reorder = (sourceIndex: number, destinationIndex: number) => {
+    const tasks = [...taskList];
+    const [removed] = tasks.splice(sourceIndex, 1);
+
+    tasks.splice(destinationIndex, 0, removed);
+
+    return tasks;
+  };
+
   const handleDragEnd = (e: any) => {
-    console.log(e);
-    console.log("drag ended");
+    const { source, destination } = e;
+
+    const newTaskList = reorder(source.index, destination.index);
+
+    setTaskList(newTaskList);
   };
 
   return (
@@ -14,7 +29,11 @@ function App() {
       <DragDropContext onDragEnd={handleDragEnd}>
         <Grid columns="repeat(4, minmax(180px, 250px))" gap={3}>
           <ListTask />
-          <ListTask title="Task List 2" id="task-droppable-2" />
+          <ListTask
+            title="Task List 2"
+            id="task-droppable-2"
+            tasks={taskList}
+          />
           <ListTask title="Task List 4" id="task-droppable-3" />
           <ListTask title="Task List 3" id="task-droppable-4" />
         </Grid>

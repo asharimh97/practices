64fb626e5b83528c683d4019c03c2c4c4ed33194
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-12-01 20:35:08 +0700

    Replace all local state to redux state
    Project finished

--- react-practices/kanban-typescript/src/App.tsx
+++ react-practices/kanban-typescript/src/App.tsx
@@ -1,13 +1,14 @@
-import React, { useEffect, useState } from "react";
+import React, { useState } from "react";
 import { Box, Button, Container, Grid } from "components/atoms";
 import { DragDropContext } from "react-beautiful-dnd";
 import ListTask from "components/organisms/ListTask";
-import tasks from "__mocks__/tasks";
 import Modal from "components/molecules/Modal";
 import { connect } from "react-redux";
 import { listSelector } from "stores/lists/reducer";
 import { bindActionCreators } from "redux";
-import { addList } from "stores/actions";
+import { addList, moveTask } from "stores/actions";
+import FormAddList from "components/organisms/FormAddList";
+import { propType as listPropType } from "stores/lists/schema";
 
 type Props = {
   [key: string]: any;
@@ -15,121 +16,35 @@ type Props = {
 
 function App({ ...props }: Props) {
   const [modal, setModal] = useState(false);
-  const taskListIds = [
-    "task-droppable",
-    "task-droppable-2",
-    "task-droppable-3"
-  ];
 
-  const mapTaskList = taskListIds.reduce(
-    (mapTasks: Record<string, any>, current) => {
-      mapTasks[current] = tasks;
-
-      return mapTasks;
-    },
-    {}
-  );
-
-  const [taskList, setTaskList] = useState<Record<string, any>>({});
-
-  useEffect(() => {
-    setTaskList(mapTaskList);
-    // eslint-disable-next-line react-hooks/exhaustive-deps
-  }, []);
-
-  // Only applies to same list
-  const reorder = (
-    list: Array<any>,
-    sourceIndex: number,
-    destinationIndex: number
-  ) => {
-    const tasks = [...list];
-    const [removed] = tasks.splice(sourceIndex, 1);
-
-    tasks.splice(destinationIndex, 0, removed);
-
-    return tasks;
-  };
-
-  const move = (
-    sourceList: Array<any>,
-    destinationList: Array<any>,
-    sourceIndex: number,
-    destinationIndex: number
-  ) => {
-    const tasks1 = [...sourceList];
-    const tasks2 = [...destinationList];
-
-    const [removedItem] = tasks1.splice(sourceIndex, 1);
-
-    tasks2.splice(destinationIndex, 0, removedItem);
-
-    const result = {
-      source: tasks1,
-      destination: tasks2
-    };
-
-    return result;
-  };
-
-  const getList = (id: string) => taskList[id];
-
-  const handleAdd = (val: Record<string, any>) => {
-    const listId = taskListIds[0];
-    const list = [...taskList[listId]];
-    list.push(val);
-
-    setTaskList({
-      ...taskList,
-      [listId]: list
-    });
-
-    props.addList(val.task);
-
+  const handleAddList = (val: Record<string, any>, e: any) => {
+    props.addList(val.name);
     setModal(false);
+    e.target.reset();
   };
 
   const handleDragEnd = (e: any) => {
     const { source, destination } = e;
 
-    if (source.droppableId === destination.droppableId) {
-      const newTaskList = reorder(
-        taskList[source.droppableId],
-        source.index,
-        destination.index
-      );
+    console.log({ source, destination });
+    const data = {
+      sourceList: source.droppableId,
+      destList: destination.droppableId,
+      sourceIndex: source.index,
+      destIndex: destination.index
+    };
 
-      setTaskList({
-        ...taskList,
-        [source.droppableId]: newTaskList
-      });
-    } else {
-      const res = move(
-        getList(source.droppableId),
-        getList(destination.droppableId),
-        source.index,
-        destination.index
-      );
-
-      setTaskList({
-        ...taskList,
-        [source.droppableId]: res.source,
-        [destination.droppableId]: res.destination
-      });
-    }
+    props.moveTask(data);
   };
+
+  console.log(props.list);
 
   return (
     <Container>
       <DragDropContext onDragEnd={handleDragEnd}>
         <Grid columns="repeat(4, minmax(180px, 250px))" gap={3}>
-          {taskListIds.map((listId, idx) => (
-            <ListTask
-              key={idx}
-              id={listId}
-              title={`Task List #${idx}`}
-              tasks={taskList[listId]}
-            />
+          {props.list.map(({ id, name }: listPropType) => (
+            <ListTask key={id} id={id} title={`List: ${name}`} />
           ))}
         </Grid>
       </DragDropContext>
@@ -138,7 +53,7 @@ function App({ ...props }: Props) {
         visible={modal}
         onClose={() => setModal(false)}
       >
-        Disini nanti isinya form add list
+        <FormAddList onSubmit={handleAddList} />
       </Modal>
       <Box position="fixed" top={3} right={3} onClick={() => setModal(true)}>
         <Button colorType="gradient" color="blue" rounded>
@@ -150,11 +65,11 @@ function App({ ...props }: Props) {
 }
 
 const mapStateToProps = (state: any) => ({
-  list: state.list,
-  listSelected: listSelector(state)
+  list: listSelector(state)
 });
 const mapDispatchToProps = (dispatch: any) => ({
-  addList: bindActionCreators(addList, dispatch)
+  addList: bindActionCreators(addList, dispatch),
+  moveTask: bindActionCreators(moveTask, dispatch)
 });
 
 export default connect(mapStateToProps, mapDispatchToProps)(App);

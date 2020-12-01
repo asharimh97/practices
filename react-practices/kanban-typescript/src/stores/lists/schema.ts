2b8ae7a556170ccb64639d9c2221248e2fe95a50
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-12-01 20:33:37 +0700

    Update list schema

--- react-practices/kanban-typescript/src/stores/lists/schema.ts
+++ react-practices/kanban-typescript/src/stores/lists/schema.ts
@@ -1,13 +1,9 @@
-import { propType as taskPropType } from "../tasks/schema";
-
 export interface propType {
   id: string | number;
   name: string;
-  tasks: Array<taskPropType>;
 }
 
 export const schema = {
   id: "",
-  name: "",
-  tasks: []
+  name: ""
 };

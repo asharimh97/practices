a4d618e6ba8ef278ca4a550ce0f23c240d7dcf7e
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-12-04 21:17:58 +0700

    Try on props and methods

--- vue-practices/first-app/src/App.vue
+++ vue-practices/first-app/src/App.vue
@@ -1,7 +1,7 @@
 <template>
   <img alt="Vue logo" src="./assets/logo.png">
   <HelloWorld msg="Welcome to Your Vue.js App"/>
-  <Sembarang />
+  <Sembarang :id="12" />
 </template>
 
 <script>
--- vue-practices/first-app/src/components/Sembarang.vue
+++ vue-practices/first-app/src/components/Sembarang.vue
@@ -1,11 +1,37 @@
 <template>
-  <div>
-    Halo ini component sembarangan
+  <div :title="`${id} lorem ipsum dolorsit amet`">
+    Halo ini component sembarangan {{ name ? name : "kosong ges" }}
   </div>
+  <div>
+    {{ message }}
+  </div>
+  <button v-on:click="handleChangeMessage">
+    Change the message buddy!
+  </button>
 </template>
 <script>
+const alternateNames = [
+  "Ipsum lorem dolorsit amet",
+  "habeda habeda habeda",
+  "ini nama yang lain",
+  "apakah kamu kepikiran ide nama lainnya kawan",
+  "kalo ini gimana ges"
+];
+
 export default {
-  name: "Sembarang"
+  name: "Sembarang",
+  data: () => ({
+    message: "Lorem ipsum dolorsit amet"
+  }),
+  props: ['name', 'id'],
+  methods: {
+    handleChangeMessage() {
+      const randomIndex = Math.round(Math.random() * 100) % alternateNames.length;
+      const newMessage = alternateNames[randomIndex];
+
+      this.message = newMessage;
+    }
+  }
 }
 </script>
 <style>

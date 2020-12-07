d2ee84baed6e9825cbcc49b36b733942e84f9753
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-12-07 19:42:22 +0700

    Add navbar component

--- vue-practices/planina/src/App.vue
+++ vue-practices/planina/src/App.vue
@@ -1,27 +1,59 @@
 <template>
   <div>
-    <div id="app">Welcome to vue app baby! <ion-icon name="heart" /></div>
-    <Sembarang> Ini konten sembarang kawankuh </Sembarang>
+    <div class="background" />
+    <Navbar />
   </div>
 </template>
 
 <script>
-import Sembarang from "./components/Sembarang";
+import Navbar from "./components/molecules/Navbar";
+
 export default {
   name: "App",
   components: {
-    Sembarang
+    Navbar
   }
 };
 </script>
 
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+<style lang="scss">
+:root {
+  --white: #fff;
+  --black: #000;
+  --dark-grey: #666;
+
+  /* Font weight */
+  --light: 300;
+  --regular: 400;
+  --medium: 500;
+  --semibold: 600;
+  --bold: 700;
+  --extrabold: 800;
+}
+* {
+  box-sizing: border-box;
+}
+
+html,
+body {
+  font-family: "Manrope", -apple-system, BlinkMacSystemFont, "Segoe UI", Roboto,
+    Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif;
+  left: 0;
+  margin: 0;
+  padding: 0;
+  top: 0;
+}
+.background {
+  background: linear-gradient(
+      180deg,
+      rgba(0, 0, 0, 0.4) 0%,
+      rgba(0, 0, 0, 0) 100%
+    ),
+    url("/images/mountain.jpg");
+  background-position: center;
+  background-size: cover;
+  height: 100%;
+  position: fixed;
+  width: 100%;
 }
 </style>
---
+++ vue-practices/planina/src/components/molecules/Navbar.vue
@@ -0,0 +1,62 @@
+<template>
+  <header class="navbar">
+    <Container>
+      <nav class="navigation">
+        <a href="/" class="navigation__brand"> Planina </a>
+
+        <p class="navigation__profile">
+          Howdy, {{ username }} <ion-icon name="person" />
+        </p>
+      </nav>
+    </Container>
+  </header>
+</template>
+<script>
+import Container from "../atoms/Container";
+
+export default {
+  name: "Navbar",
+  components: {
+    Container
+  },
+  props: {
+    username: {
+      type: String,
+      default: "Antelope"
+    }
+  }
+};
+</script>
+<style lang="scss">
+.navbar {
+  padding: 40px 0;
+  position: fixed;
+  width: 100%;
+}
+.navigation {
+  align-items: center;
+  display: flex;
+  justify-content: space-between;
+}
+.navigation__brand {
+  color: var(--white);
+  font-size: 20px;
+  font-weight: var(--medium);
+  letter-spacing: 5px;
+  line-height: 27px;
+  text-transform: uppercase;
+  text-decoration: none;
+}
+.navigation__profile {
+  color: var(--white);
+  font-size: 16px;
+  font-weight: var(--medium);
+  letter-spacing: 2px;
+  line-height: 22px;
+  margin: 0;
+
+  ion-icon {
+    margin-left: 16px;
+  }
+}
+</style>

d8507a1f187af5f18b69a00ab83c0302b451c151
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-12-08 20:04:57 +0700

    Add main content component

---
+++ vue-practices/planina/src/components/atoms/MainContent.vue
@@ -0,0 +1,19 @@
+<template>
+  <main class="main-content">
+    <slot></slot>
+  </main>
+</template>
+<script>
+export default {
+  name: "MainContent"
+};
+</script>
+<style scoped lang="scss">
+.main-content {
+  align-items: center;
+  display: flex;
+  min-height: calc(100vh - 102px);
+  position: relative;
+  z-index: 5;
+}
+</style>

941b8192dba0066d72375ce8dd25bc9a5f6e6a67
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-12-09 10:24:38 +0700

    Add flex component

---
+++ vue-practices/planina/src/components/atoms/Flex.vue
@@ -0,0 +1,20 @@
+<template>
+  <div class="flex">
+    <slot></slot>
+  </div>
+</template>
+<script>
+export default {
+  name: "Flex"
+};
+</script>
+<style scoped lang="scss">
+.flex {
+  align-items: center;
+  display: flex;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+}
+</style>

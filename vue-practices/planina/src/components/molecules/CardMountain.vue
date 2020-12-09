5a4c34dcb94f91512c4acc5159b97a1da46a0c0c
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-12-09 10:24:55 +0700

    Add card mountain and list card mountain

---
+++ vue-practices/planina/src/components/molecules/CardMountain.vue
@@ -0,0 +1,121 @@
+<template>
+  <div class="card-mountain" :class="{ 'card-mountain--active': isActive }">
+    <div class="card-mountain__image">
+      <img :src="image" :alt="location" />
+    </div>
+    <div class="card-mountain__overlay" />
+    <div class="card-mountain__content">
+      <p class="card-mountain__city">
+        {{ city }}
+      </p>
+      <p class="card-mountain__location">
+        {{ location }}
+      </p>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  name: "CardMountain",
+  props: {
+    city: {
+      type: String,
+      default: "Wonosobo"
+    },
+    location: {
+      type: String,
+      default: "Kembang"
+    },
+    image: {
+      type: String,
+      default: "/images/mountain.jpg"
+    },
+    isActive: {
+      type: Boolean,
+      default: false
+    }
+  }
+};
+</script>
+<style scoped lang="scss">
+.card-mountain {
+  border-radius: 10px;
+  cursor: pointer;
+  flex: 0 0 auto;
+  height: 380px;
+  margin-right: 24px;
+  position: relative;
+  width: 255px;
+  transition: all 0.3s ease-in-out;
+
+  &:hover,
+  &.card-mountain--active {
+    .card-mountain__overlay {
+      opacity: 1;
+    }
+  }
+
+  &.card-mountain--active {
+    margin-left: 20px;
+    margin-right: 32px;
+    transform: scale(1.1);
+  }
+}
+.card-mountain__image {
+  height: 100%;
+  position: relative;
+  width: 100%;
+
+  & > img {
+    border-radius: 10px;
+    height: 100%;
+    object-fit: cover;
+    object-position: center;
+    position: absolute;
+    width: 100%;
+  }
+}
+.card-mountain__overlay {
+  background: linear-gradient(
+    180deg,
+    rgba(0, 0, 0, 0) 0%,
+    rgba(0, 0, 0, 0.65) 100%
+  );
+  border-radius: 10px;
+  height: 100%;
+  left: 0;
+  opacity: 0.7;
+  position: absolute;
+  top: 0;
+  transition: all 0.3s ease-in-out;
+  width: 100%;
+}
+.card-mountain__content {
+  display: flex;
+  flex-direction: column;
+  justify-content: flex-end;
+  height: 100%;
+  left: 0;
+  top: 0;
+  padding: 20px;
+  position: absolute;
+  width: 100%;
+
+  p {
+    margin: 0;
+    color: var(--white);
+    text-transform: uppercase;
+  }
+
+  .card-mountain__city {
+    font-size: 13px;
+    line-height: 18px;
+  }
+
+  .card-mountain__location {
+    font-size: 32px;
+    font-weight: var(--extrabold);
+    line-height: 44px;
+  }
+}
+</style>
---
+++ vue-practices/planina/src/components/molecules/ListCardMountain.vue
@@ -0,0 +1,45 @@
+<template>
+  <div class="list-card-mountain">
+    <CardMountain
+      location="Merapi"
+      city="Yogyakarta"
+      :is-active="true"
+      image="/images/mountain-1.jpg"
+    />
+    <CardMountain
+      location="Sumbing"
+      city="Wonosobo"
+      image="/images/mountain-2.jpg"
+    />
+    <CardMountain
+      location="Prau"
+      city="Wonosobo"
+      image="/images/mountain-3.jpg"
+    />
+    <CardMountain
+      location="Andong"
+      city="Magelang"
+      image="/images/mountain-4.jpg"
+    />
+  </div>
+</template>
+<script>
+import CardMountain from "./CardMountain";
+export default {
+  name: "ListCardMountain",
+  components: { CardMountain }
+};
+</script>
+<style scoped lang="scss">
+.list-card-mountain {
+  align-items: center;
+  display: flex;
+  flex-wrap: nowrap;
+  margin-right: -40px;
+  overflow-x: auto;
+
+  &::-webkit-scrollbar {
+    display: none;
+  }
+}
+</style>

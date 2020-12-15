eb8de9c25d31238883a419d6bc380e1ec87051b6
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-12-15 21:35:31 +0700

    Add basic layout for mountains page

---
+++ css-practices/mountains/script.js
@@ -0,0 +1,9 @@
+const fullPage = new fullpage("#fullpage", {
+  // write options here
+  scrollBar: false,
+  // set content vertically cented by default
+  verticalCentered: true,
+  // navigation
+  navigation: true,
+  navigationPosition: "left",
+});

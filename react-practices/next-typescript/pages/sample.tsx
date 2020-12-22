ecd79a995930551770ec9d92b8a143b07a7baa95
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-12-22 20:38:25 +0700

    Implement ts with baseUrl

---
+++ react-practices/next-typescript/pages/sample.tsx
@@ -0,0 +1,12 @@
+import Sample from "../src/components/Sample";
+
+function SamplePage() {
+  return (
+    <div>
+      <Sample />
+      <p>Apaan nih gan</p>
+    </div>
+  )
+}
+
+export default SamplePage;
---
+++ react-practices/next-typescript/src/components/Sample.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+const Sample: React.FC = () => {
+  return (
+    <div>Sample component</div>
+  )
+}
+
+export default Sample;

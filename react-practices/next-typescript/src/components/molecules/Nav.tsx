c447770268f2e177768e7549fada5d15e7ab2e72
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-12-31 17:18:16 +0700

    Update Nav component's style

--- react-practices/next-typescript/src/components/molecules/Nav.tsx
+++ react-practices/next-typescript/src/components/molecules/Nav.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Link from "next/link";
 import { Flex } from "components/atoms";
+import theme from "styles/theme";
+import styled from "styled-components";
 
 type NavProp = {
   icon: React.ReactNode;
@@ -8,15 +10,36 @@ type NavProp = {
   children: React.ReactNode;
 };
 
+const NavWrapper = styled(Flex).attrs({
+  align: "center",
+  px: 3
+})`
+  border-left: solid 1px;
+  border-color: ${theme.colors.black["40"]};
+
+  &:last-child {
+    border-right: solid 1px ${theme.colors.black["40"]};
+  }
+`;
+
 const Nav: React.FC<NavProp> = ({ icon, href, children }) => {
+  const renderIcon = Icon => (
+    <Icon
+      data-testid="nav-icon"
+      style={{ color: "#B75DEF", marginRight: "8px" }}
+    />
+  );
+
   return (
-    <Link href={href}>
-      <a>
-        <Flex align="center">
-          {icon} {children}
-        </Flex>
-      </a>
-    </Link>
+    <NavWrapper>
+      <Link href={href}>
+        <a>
+          <Flex>
+            {renderIcon(icon)} {children}
+          </Flex>
+        </a>
+      </Link>
+    </NavWrapper>
   );
 };
 

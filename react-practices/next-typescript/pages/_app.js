efda20f480e2e0d3e0d1b9a635e9e155bf4c9301
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-12-31 17:17:57 +0700

    Add ThemeProvider

--- react-practices/next-typescript/pages/_app.js
+++ react-practices/next-typescript/pages/_app.js
@@ -1,8 +1,14 @@
 import { wrapper } from "stores";
+import { ThemeProvider } from "styled-components";
+import theme from "styles/theme";
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />;
+  return (
+    <ThemeProvider theme={theme}>
+      <Component {...pageProps} />
+    </ThemeProvider>
+  );
 }
 
 export default wrapper.withRedux(MyApp);

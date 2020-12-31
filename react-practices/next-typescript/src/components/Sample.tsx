587540228cff91876bc5854e67e4bbb6f65b8089
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2020-12-31 17:18:25 +0700

    Add components

--- react-practices/next-typescript/src/components/Sample.tsx
+++ react-practices/next-typescript/src/components/Sample.tsx
@@ -1,8 +1,33 @@
 import React from "react";
-import { Container } from "./atoms";
+import { IoIosAirplane } from "react-icons/io";
+import { Container, Flex, Grid } from "./atoms";
+import Sembarang from "./atoms/Sembarang";
+import Nav from "./molecules/Nav";
 
 const Sample: React.FC = () => {
-  return <Container>Sample component</Container>;
+  return (
+    <div>
+      <Container>Sample component</Container>
+      <Flex>
+        <Nav href="/" icon={IoIosAirplane}>
+          Sample Nav
+        </Nav>
+        <Nav href="/" icon={IoIosAirplane}>
+          Sample Nav
+        </Nav>
+        <Nav href="/" icon={IoIosAirplane}>
+          Sample Nav
+        </Nav>
+        <Nav href="/" icon={IoIosAirplane}>
+          Sample Nav
+        </Nav>
+      </Flex>
+      <Sembarang mx="14px">
+        X styled enggak cocok untuk project berbasis typescript, type
+        definitionnya belum lengkap, hmm
+      </Sembarang>
+    </div>
+  );
 };
 
 export default Sample;

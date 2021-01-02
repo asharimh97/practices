f60e24bd663fd28313691dfc255bdcea3b5cfc92
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-01-02 23:41:57 +0700

    Add css styles

--- react-practices/next-typescript/pages/_app.js
+++ react-practices/next-typescript/pages/_app.js
@@ -1,6 +1,10 @@
 import { wrapper } from "stores";
 import { ThemeProvider } from "styled-components";
 import theme from "styles/theme";
+
+import "../src/styles/normalize.css";
+import "../src/styles/style.css";
+
 import "../styles/globals.css";
 
 function MyApp({ Component, pageProps }) {

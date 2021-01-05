00069b041d899520ca4a388442431be538254584
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-01-06 05:29:28 +0700

    Play with typescript and tsconfig

---
+++ typescript/lesson-1/test.ts
@@ -0,0 +1,17 @@
+const test: number = 10;
+
+type SembarangType = {
+  name: string;
+  address: {
+    location: string;
+    city: string;
+  }
+};
+
+const lorem: SembarangType = {
+  name: "John Doe",
+  address: {
+    location: "Jawa tengah",
+    city: "Semarang"
+  }
+}

393adcb33c74c0f66a75d962457b86df05c5d474
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-01-16 20:01:12 +0700

    Update ActionPayload implementation

--- react-practices/next-typescript/src/stores/types.ts
+++ react-practices/next-typescript/src/stores/types.ts
@@ -1,10 +1,10 @@
-interface PayloadThunk {
-  data?: Record<string, any>;
+import { PayloadAction } from "@reduxjs/toolkit";
+
+interface PayloadThunk<T = any> {
+  data?: T;
   error?: Record<string, any> | string;
   message?: string;
 }
 
-export interface ActionPayload {
-  type: string;
-  payload: PayloadThunk;
-}
+export interface ActionPayload<T = any>
+  extends PayloadAction<PayloadThunk<T>> {}

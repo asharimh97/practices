a515da3fb4fe1c6ccb07e49299258b2de2ea745b
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-01-17 11:46:17 +0700

    Learn return type works

---
+++ typescript/lesson-3/lesson-six.ts
@@ -0,0 +1,20 @@
+let userId = 0;
+function createUser(name: string, role: 'admin' | 'member' | 'maintainer', isActive: boolean) {
+  return {
+    userId: userId++,
+    name,
+    role,
+    isActive,
+    createdAt: new Date()
+  }
+}
+
+type GetReturn<T> = T extends (...args: any[]) => infer T ? T : never
+
+// get the return type of a function or library, based on its params
+type UserType = GetReturn<typeof createUser>
+// equals to
+type User = ReturnType<typeof createUser>
+
+// Generates parameter of a function in array
+type UserParam = Parameters<typeof createUser>

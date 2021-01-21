778e56f695ee6f807001b2756e91412d02534867
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-01-21 18:44:27 +0700

    Add theme and styles

---
+++ react-practices/blueathue-ts/src/styles/theme.ts
@@ -0,0 +1,10 @@
+export const fonts = {
+  body: `"Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
+Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif`
+};
+
+const theme = {
+  fonts
+};
+
+export default theme;

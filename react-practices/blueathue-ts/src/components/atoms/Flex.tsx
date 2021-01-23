38b159322ba9bb5f6f754a1539c7fbacde57c7b8
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-01-23 19:58:39 +0700

    Update flex properties

--- react-practices/blueathue-ts/src/components/atoms/Flex.tsx
+++ react-practices/blueathue-ts/src/components/atoms/Flex.tsx
@@ -1,12 +1,37 @@
-import { AnyComponentProp } from "../../../@types/general";
-import Base from "./Base";
+import { FlexboxesProps, style, x } from "@xstyled/styled-components";
+import { BaseStyledComponentProp } from "../../../@types/general";
 
-const Flex: React.FC<AnyComponentProp> = ({ children, ...props }) => {
-  return (
-    <Base {...props} display="flex">
-      {children}
-    </Base>
-  );
+const flexProps = [
+  {
+    prop: "align",
+    css: "alignItems"
+  },
+  {
+    prop: "justify",
+    css: "justifyContent"
+  },
+  {
+    prop: "direction",
+    css: "flexDirection"
+  }
+];
+
+const mappedFlexProps = flexProps.map(({ prop, css }) =>
+  style({ prop, cssProperty: css })
+);
+
+const Comp = x.extend(...mappedFlexProps);
+
+interface FlexProp extends FlexboxesProps, BaseStyledComponentProp {
+  children?: React.ReactNode;
+  align?: string | object | string[] | number | number[];
+  justify?: string | object | string[] | number | number[];
+  direction?: string | object | string[] | number | number[];
+  [key: string]: any;
+}
+
+const Flex: React.FC<FlexProp> = props => {
+  return <Comp.div {...props} display="flex" />;
 };
 
 export default Flex;

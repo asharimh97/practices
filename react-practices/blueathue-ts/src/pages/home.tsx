e77b34babc116b62b5c057d1dba25953a7849fe7
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-01-23 19:58:48 +0700

    Playing with new Flex component

--- react-practices/blueathue-ts/src/pages/home.tsx
+++ react-practices/blueathue-ts/src/pages/home.tsx
@@ -2,12 +2,11 @@ import { Flex } from "../components/atoms";
 
 function Home() {
   return (
-    <Flex>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
-      <p>Home</p>
+    <Flex w="100%" align="center" justify="space-between">
+      <p style={{ marginTop: "20px" }}>Home</p>
+      <p style={{ marginTop: "40px" }}>Home</p>
+      <p style={{ marginTop: "30px" }}>Home</p>
+      <p style={{ marginTop: "0px" }}>Home</p>
     </Flex>
   );
 }

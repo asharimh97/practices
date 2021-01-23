8e74bcc18d2e283195be5e3532b7a9a3f71c28b6
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-01-23 19:58:31 +0700

    Add base style component interface

--- react-practices/blueathue-ts/@types/general.d.ts
+++ react-practices/blueathue-ts/@types/general.d.ts
@@ -1,3 +1,21 @@
+import {
+  BordersProps,
+  ColorProps,
+  ITheme,
+  LayoutProps,
+  SizingProps,
+  SpaceProps,
+  Theme,
+  TransitionProps
+} from "@xstyled/styled-components";
+
 export interface AnyComponentProp {
   [key: string]: any;
 }
+
+export type BaseStyledComponentProp<T extends ITheme = Theme> = SpaceProps<T> &
+  LayoutProps<T> &
+  ColorProps<T> &
+  TransitionProps<T> &
+  SizingProps<T> &
+  BordersProps<T>;

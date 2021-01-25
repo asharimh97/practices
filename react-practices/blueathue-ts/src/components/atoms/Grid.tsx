decb7615e1f479d7d81dabc0bb56dd37bc25bb17
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-01-25 20:12:01 +0700

    Add grid component

---
+++ react-practices/blueathue-ts/src/components/atoms/Grid.tsx
@@ -0,0 +1,25 @@
+import { GridsProps, style, x } from "@xstyled/styled-components";
+import { BaseStyledComponentProp } from "../../../@types/general";
+
+const gridProps = [
+  { prop: "column", css: "gridTemplateColumns" },
+  { prop: "row", css: "gridTemplateRows" }
+];
+
+const mappedGridProps = gridProps.map(({ prop, css }) =>
+  style({ prop, cssProperty: css })
+);
+
+const Comp = x.extend(...mappedGridProps);
+
+interface GridProp extends BaseStyledComponentProp, GridsProps {
+  column?: string | object | string[] | number | number[];
+  row?: string | object | string[] | number | number[];
+}
+
+const Grid: React.FC<GridProp> = props => {
+  // eslint-disable-next-line react/jsx-pascal-case
+  return <Comp.div {...props} display="grid" />;
+};
+
+export default Grid;

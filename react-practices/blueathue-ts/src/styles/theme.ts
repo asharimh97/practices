1d5417b01fbeccfe1dc987a20a5dd277ec75a9d2
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-01-26 21:48:06 +0700

    Update theme

--- react-practices/blueathue-ts/src/styles/theme.ts
+++ react-practices/blueathue-ts/src/styles/theme.ts
@@ -1,6 +1,11 @@
+import { defaultTheme } from "@xstyled/styled-components";
+
 export const fonts = {
+  ...defaultTheme.fonts,
   body: `"Source Sans Pro", -apple-system, BlinkMacSystemFont, "Segoe UI",
-Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif`
+Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif`,
+  montserrat: `"Montserrat", -apple-system, BlinkMacSystemFont, "Segoe UI", 
+  Roboto, Oxygen, Ubuntu, Cantarell, "Open Sans", "Helvetica Neue", sans-serif`
 };
 
 const theme = {

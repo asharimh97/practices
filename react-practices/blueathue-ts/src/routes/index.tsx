ae9a331baa595b7dfa6c27c1d254d82b942c50ae
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-01-26 21:47:57 +0700

    Replace routing with constant

--- react-practices/blueathue-ts/src/routes/index.tsx
+++ react-practices/blueathue-ts/src/routes/index.tsx
@@ -1,13 +1,7 @@
 import { Route, Switch, BrowserRouter as Router } from "react-router-dom";
-import Contact from "../pages/contact";
 import Home from "../pages/home";
-import Life from "../pages/life";
-import Mode from "../pages/mode";
-import MyTwoCents from "../pages/my-two-cents";
-import People from "../pages/people";
-import Places from "../pages/places";
-import Quotes from "../pages/quotes";
 import ErrorPage from "../pages/_error";
+import { PAGE_ROUTES } from "../utils/constants";
 
 export default function AppRoutes() {
   return (
@@ -16,31 +10,12 @@ export default function AppRoutes() {
         <Route path="/" exact>
           <Home />
         </Route>
-        <Route path="/my-two-cents">
-          <MyTwoCents />
-        </Route>
-        <Route path="/my-two-cents">
-          <MyTwoCents />
-        </Route>
-        <Route path="/quotes">
-          <Quotes />
-        </Route>
-        <Route path="/mode">
-          <Mode />
-        </Route>
 
-        <Route path="/places">
-          <Places />
-        </Route>
-        <Route path="/people">
-          <People />
-        </Route>
-        <Route path="/life">
-          <Life />
-        </Route>
-        <Route path="/contact">
-          <Contact />
-        </Route>
+        {PAGE_ROUTES.map(({ link, page: Page }, idx) => (
+          <Route path={link} exact key={idx}>
+            <Page />
+          </Route>
+        ))}
 
         <Route path="*">
           <ErrorPage />

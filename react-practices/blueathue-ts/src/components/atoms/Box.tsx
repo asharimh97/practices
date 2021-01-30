c36e22acb8332ac5e87d99b3c74ec465a58f072d
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-01-30 17:58:07 +0700

    Ignore ts error

--- react-practices/blueathue-ts/src/components/atoms/Box.tsx
+++ react-practices/blueathue-ts/src/components/atoms/Box.tsx
@@ -1,12 +1,14 @@
 import { x } from "@xstyled/styled-components";
 import React from "react";
+import { BaseStyledComponentProp } from "../../../@types/general";
 
-interface BoxProp {
+interface BoxProp extends BaseStyledComponentProp {
   children?: React.ReactNode;
   [key: string]: any;
 }
 
 const Box: React.FC<BoxProp> = ({ children, ...props }) => {
+  // @ts-ignore
   return <x.div {...props}>{children}</x.div>;
 };
 

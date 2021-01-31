ce979d70d08b8183f241b8aa0d9b56e8f7299f0e
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-01-31 20:12:18 +0700

    Implement sample PhotoCard component to page mode

--- react-practices/blueathue-ts/src/components/organisms/mode/index.tsx
+++ react-practices/blueathue-ts/src/components/organisms/mode/index.tsx
@@ -1,5 +1,18 @@
+import PageTitle from "../../molecules/PageTitle";
+import PhotoCard from "../../molecules/PhotoCard";
+
 const ModeComponent: React.FC = () => {
-  return <div>Halo halo ini halaman mode</div>;
+  return (
+    <>
+      <PageTitle title="Mode" subtitle="What I wear for sure" />
+      <div style={{ width: "375px" }}>
+        <PhotoCard
+          src="/images/sky.jpg"
+          caption="Hujan hujan pergilah datanglah di lain hari"
+        />
+      </div>
+    </>
+  );
 };
 
 export default ModeComponent;

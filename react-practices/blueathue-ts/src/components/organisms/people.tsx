cbceeb84d4fd331096fb57ee86d2cc9d583a3618
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-02-01 21:02:38 +0700

    Update people page to be blog list page

--- react-practices/blueathue-ts/src/components/organisms/people.tsx
+++ react-practices/blueathue-ts/src/components/organisms/people.tsx
@@ -1,37 +1,36 @@
-// @ts-ignore
-import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
+import { randomDate } from "../../utils/helpers";
+import { Grid } from "../atoms";
+import BlogCard from "../molecules/BlogCard";
 import PageTitle from "../molecules/PageTitle";
-import PhotoCard from "../molecules/PhotoCard";
 
 const PeopleComponent: React.FC = () => {
-  const responsiveBreakpoints = {
-    320: 1,
-    768: 2,
-    992: 3,
-    1200: 4
-  };
   return (
     <>
       <PageTitle
         title="People"
         subtitle="People I have met, I adore, and I love"
       />
-      <div>
-        <ResponsiveMasonry columnsCountBreakPoints={responsiveBreakpoints}>
-          <Masonry gutter="24px">
-            {[...new Array(12)].map((_, idx) => {
-              const imageIdx = Math.floor(((Math.random() * 10) % 4) + 1);
-              return (
-                <PhotoCard
-                  src={`/images/people-${imageIdx}.jpg`}
-                  key={idx}
-                  caption="Lorem ipsum dolorsit amet"
-                />
-              );
-            })}
-          </Masonry>
-        </ResponsiveMasonry>
-      </div>
+      <Grid
+        column={{
+          _: "repeat(1, 1fr)",
+          md: "repeat(1, 2fr)",
+          lg: "repeat(3, 1fr)",
+          xl: "repeat(4, 1fr)"
+        }}
+        gap="24px"
+      >
+        {[...new Array(12)].map((_, idx) => {
+          const imageIdx = Math.floor(((Math.random() * 10) % 4) + 1);
+          const blogDate = randomDate(new Date(2018, 0, 1), new Date());
+          return (
+            <BlogCard
+              title="Lorem Ipsum dolorsit Amet"
+              date={blogDate}
+              cover={`/images/people-${imageIdx}.jpg`}
+            />
+          );
+        })}
+      </Grid>
     </>
   );
 };

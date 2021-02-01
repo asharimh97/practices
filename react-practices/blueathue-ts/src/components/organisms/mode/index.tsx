45e4768c42cd19af3fed46153f19bb9a91ed9183
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-02-01 20:03:28 +0700

    Implement masonry to mode page

--- react-practices/blueathue-ts/src/components/organisms/mode/index.tsx
+++ react-practices/blueathue-ts/src/components/organisms/mode/index.tsx
@@ -1,15 +1,33 @@
+// @ts-ignore
+import Masonry, { ResponsiveMasonry } from "react-responsive-masonry";
 import PageTitle from "../../molecules/PageTitle";
 import PhotoCard from "../../molecules/PhotoCard";
 
 const ModeComponent: React.FC = () => {
+  const responsiveBreakpoints = {
+    320: 1,
+    768: 2,
+    992: 3,
+    1200: 4
+  };
   return (
     <>
       <PageTitle title="Mode" subtitle="What I wear for sure" />
-      <div style={{ width: "375px" }}>
-        <PhotoCard
-          src="/images/sky.jpg"
-          caption="Hujan hujan pergilah datanglah di lain hari"
-        />
+      <div>
+        <ResponsiveMasonry columnsCountBreakPoints={responsiveBreakpoints}>
+          <Masonry gutter="24px">
+            {[...new Array(12)].map((_, idx) => {
+              const imageIdx = Math.floor(((Math.random() * 10) % 4) + 1);
+              return (
+                <PhotoCard
+                  src={`/images/outfit-${imageIdx}.jpg`}
+                  key={idx}
+                  caption="Lorem ipsum dolorsit amet"
+                />
+              );
+            })}
+          </Masonry>
+        </ResponsiveMasonry>
       </div>
     </>
   );

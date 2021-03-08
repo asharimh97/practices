7d1db9a4fa9a704e4caab0588267550c346e2623
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-03-08 19:33:43 +0700

    Add axios request config

---
+++ react-practices/query/src/services/request.js
@@ -0,0 +1,24 @@
+import axios from "axios";
+
+const API_KEY = process.env.REACT_APP_OMDB_API_KEY;
+
+const request = axios.create({});
+
+request.interceptors.request.use(config => {
+  if (config.url.includes("omdbapi.com")) {
+    config.params.apikey = API_KEY;
+  }
+
+  return config;
+});
+
+request.interceptors.response.use(response => {
+  let res;
+  if (response.status === 200) {
+    res = response.data
+  }
+
+  return res;
+})
+
+export default request;

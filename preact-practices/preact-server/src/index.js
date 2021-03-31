7ce27ad7ec1e6144084f68901b41ca43271daeb0
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-03-31 21:37:56 +0700

    Update server websocket to primus

--- preact-practices/preact-server/src/index.js
+++ preact-practices/preact-server/src/index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const app = express();
 const PORT = 4000;
 
+const fs = require("fs");
+
 const Primus = require("primus");
 const cors = require("cors");
 
@@ -14,32 +16,45 @@ app.get("/", (req, res) => {
 
 // Create server
 const server = require("http").createServer(app);
-// const primus = new Primus(server, {});
+const primus = new Primus(server, {});
 
-// // Primus websocket
-// primus.on('connection', spark => {
-//   console.log("hello ws connection");
+// Primus websocket
+primus.on('connection', spark => {
+  console.log("hello ws connection");
 
-//   spark.on("data", data => {
-//     console.log(data);
-//   })
-// });
+  // ini akan ketrigger ketika client ngasih `primus.write`
+  spark.on("data", data => {
+    console.log({ message: "retrieve data", data });
+  })
 
-// primus.on("disconnection", spark => {
-//   console.log("Uhh disconnected!");
-// });
-
-// Create websocket from socket io
-const io = require("socket.io")(server, { cors: "*" });
-
-io.on("connection", socket => {
-  console.log("connect melalui websocket");
-  socket.on("data-baru", data => {
+  spark.on("data-baru", data => {
     console.log(data);
-    io.emit("give-new-data", { data: "data baru telah dikirim" })
   })
 });
 
+primus.on("open", open => {
+  console.log("the connection is opened, alive, kicking");
+})
+
+primus.on("disconnection", spark => {
+  console.log("Uhh disconnected!");
+});
+
+// Create websocket from socket io
+// const io = require("socket.io")(server, { cors: "*" });
+
+// io.on("connection", socket => {
+//   console.log("connect melalui websocket");
+//   socket.on("data-baru", data => {
+//     console.log(data);
+//     io.emit("give-new-data", { data: "data baru telah dikirim" })
+//   })
+// });
+
 server.listen(PORT, () => {
   console.log(`Listen app on http://localhost:${PORT}`);
+  // fs.writeFile(__dirname + '/primus.js', primus.library(), err => {
+  //   if (err) throw err;
+  //   console.log("Saved!")
+  // })
 });

b5bae5a5b4643489ed872378c0c92cb9adfe2e74
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-08-31 20:29:20 +0700

    Update next config to support absolute import

--- react-practices/movie-db/next.config.js
+++ react-practices/movie-db/next.config.js
@@ -1,4 +1,12 @@
 /** @type {import('next').NextConfig} */
+
+const path = require("path");
+
 module.exports = {
   reactStrictMode: true,
-}
+  webpack5: true,
+  webpack: (config) => {
+    config.resolve.modules.push(path.resolve("./src/"));
+    return config;
+  },
+};

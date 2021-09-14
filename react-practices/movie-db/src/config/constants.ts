d14df16f4175e3fe536240e73977b8a1b433cbcc
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-09-15 05:58:29 +0700

    Add constant

---
+++ react-practices/movie-db/src/config/constants.ts
@@ -0,0 +1,12 @@
+const DEFAULT_KEYWORDS = [
+  "batman",
+  "spiderman",
+  "superman",
+  "lady",
+  "kissing",
+  "love",
+  "time",
+];
+
+// eslint-disable-next-line import/prefer-default-export
+export { DEFAULT_KEYWORDS };

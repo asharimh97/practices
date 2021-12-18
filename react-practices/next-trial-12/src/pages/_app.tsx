549dc4f160179f8e5800f1f67a5603449b6a98d5
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-12-18 13:30:33 +0700

    Move pages to src

---
+++ react-practices/next-trial-12/src/pages/_app.tsx
@@ -0,0 +1,23 @@
+import '../styles/globals.css'
+import type { AppProps } from 'next/app'
+import { defaultTheme } from "sore-ui";
+import { ThemeProvider, Preflight } from "@xstyled/styled-components"
+import { SWRConfig } from 'swr';
+import fetcher from '@/config/fetcher';
+
+interface PageProps extends AppProps {
+  fallback?: any;
+}
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <SWRConfig value={{ fetcher: fetcher }}>
+      <ThemeProvider theme={defaultTheme}>
+        <Preflight />
+        <Component {...pageProps} />
+      </ThemeProvider>
+    </SWRConfig>
+  )
+}
+
+export default MyApp

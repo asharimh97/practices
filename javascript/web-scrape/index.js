b3de4b2822c30123b314ec6918882bbaa3ff2ac2
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2021-12-22 22:45:20 +0700

    Update write scrape implementation

--- javascript/web-scrape/index.js
+++ javascript/web-scrape/index.js
@@ -3,6 +3,10 @@ const url = "http://victori.semarangkota.go.id/info";
 const cheerio = require("cheerio");
 const fs = require("fs");
 
+const cleanString = str => {
+  return String(str).trim().replace(/[\r\n]+/g," ");
+}
+
 rp(url)
   .then((html) => {
     const hoho = cheerio.load(html);
@@ -14,15 +18,15 @@ rp(url)
         if (i !== 0) {
           const parsed = el.children
             .filter(col => col.type === "tag")
-            .map(col => col.children && col.children[0] && col.children[0].data);
+            .map(col => col.children && col.children[0] && cleanString(col.children[0].data));
           // console.log(parsed)
-          parsedData.push(parsed);
+          parsedData.push(parsed.join());
         }
       })
 
       // console.log(parsedData)
 
-    fs.appendFile("log-parsed.js", parsedData, (err) => {
+    fs.appendFile("log-parsed.txt", parsedData.join("\n"), (err) => {
       if (err) {
         throw err;
       }

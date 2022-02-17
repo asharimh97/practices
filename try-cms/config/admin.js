8187efed9ed2a7170c33f41ed18a28d2e0e7a823
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2022-02-17 21:16:20 +0700

    Add api token salt

--- try-cms/config/admin.js
+++ try-cms/config/admin.js
@@ -2,4 +2,7 @@ module.exports = ({ env }) => ({
   auth: {
     secret: env('ADMIN_JWT_SECRET', 'd0776e1184154f393dd6315517fecfd0'),
   },
+  apiToken: {
+    salt: env("API_TOKEN_SALT", "food and beverages"),
+  }
 });

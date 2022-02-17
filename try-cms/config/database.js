01cb5a3e37c810131a1ab67e77b7f638e52326f8
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2022-02-17 21:16:12 +0700

    Try to change database but failed

--- try-cms/config/database.js
+++ try-cms/config/database.js
@@ -9,3 +9,20 @@ module.exports = ({ env }) => ({
     useNullAsDefault: true,
   },
 });
+
+// module.exports = ({ env }) => ({
+//   connection: {
+//     client: 'mysql',
+//     connection: {
+//       host: env('DATABASE_HOST', '127.0.0.1'),
+//       port: env.int('DATABASE_PORT', 3306),
+//       database: env('DATABASE_NAME', 'strapi'),
+//       user: env('DATABASE_USERNAME', 'ashari'),
+//       password: env('DATABASE_PASSWORD', 'ashari'),
+//       ssl: {
+//         rejectUnauthorized: env.bool('DATABASE_SSL_SELF', false), // For self-signed certificates
+//       },
+//     },
+//     debug: false,
+//   },
+// });

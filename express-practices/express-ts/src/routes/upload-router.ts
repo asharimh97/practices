9beef8be330646a40fa1f529f44533917b83d3b1
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2022-02-28 11:46:01 +0700

    Implement file filter and handle if no image

--- express-practices/express-ts/src/routes/upload-router.ts
+++ express-practices/express-ts/src/routes/upload-router.ts
@@ -1,12 +1,18 @@
+/* eslint-disable @typescript-eslint/ban-ts-comment */
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
 import path from "path";
 import StatusCodes from "http-status-codes";
 import { Router, Request, Response } from "express";
 import multer from "multer";
+import { ACCEPTED_IMAGE_FILES } from "@constants/general";
+
+// @ts-ignore
+import { ResponseAPI } from "@types/api";
 
 // Constants
 const router = Router();
-const { OK } = StatusCodes;
+const { OK, BAD_REQUEST } = StatusCodes;
 
 // Routes/paths
 const routes = {
@@ -14,6 +20,19 @@ const routes = {
 };
 
 const filePath = "../../public";
+const fileFilter = (req: Request, file: Express.Multer.File, cb: any) => {
+  if (!file) {
+    return cb(new Error("No file received"), false);
+  }
+
+  const ext = path.extname(file.originalname).toLowerCase();
+  if (!ACCEPTED_IMAGE_FILES.includes(ext)) {
+    console.log({ ext });
+    return cb(new Error("Only images are allowed"), false);
+  }
+
+  return cb(null, true);
+};
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, path.join(__dirname, filePath));
@@ -27,11 +46,17 @@ const storage = multer.diskStorage({
     cb(null, name);
   },
 });
-const upload = multer({ storage: storage });
+const upload = multer({ storage: storage, fileFilter: fileFilter });
 
-router.post(routes.index, upload.single("avatar"), (req: Request, res: Response) => {
-  console.log(req.file);
-  console.log(req.body);
+router.post(routes.index, upload.single("avatar"), (req: Request, res: Response<ResponseAPI>) => {
+  // console.log(req.file); // -> req.file is the `avatar` file
+  // console.log(req.body); // -> req.body contains the text fields, if there were any
+
+  if (!req.file) {
+    return res.status(BAD_REQUEST).json({
+      message: "No file received",
+    });
+  }
 
   res.status(OK).json({
     message: "File uploaded",

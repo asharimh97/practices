f9945f441dc72ae1bcf2db1465b15edd07838d66
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2022-03-02 19:33:13 +0700

    Update upload implementation

--- express-practices/express-ts/src/routes/upload-router.ts
+++ express-practices/express-ts/src/routes/upload-router.ts
@@ -1,14 +1,12 @@
 /* eslint-disable @typescript-eslint/ban-ts-comment */
 /* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-call */
-import path from "path";
 import StatusCodes from "http-status-codes";
 import { Router, Request, Response } from "express";
-import multer from "multer";
-import { ACCEPTED_IMAGE_FILES } from "@constants/general";
 
 // @ts-ignore
 import { ResponseAPI } from "@types/api";
+import uploadConfig from "src/config/upload-config";
 
 // Constants
 const router = Router();
@@ -19,34 +17,7 @@ const routes = {
   index: "/",
 };
 
-const filePath = "../../public";
-const fileFilter = (req: Request, file: Express.Multer.File, cb: any) => {
-  if (!file) {
-    return cb(new Error("No file received"), false);
-  }
-
-  const ext = path.extname(file.originalname).toLowerCase();
-  if (!ACCEPTED_IMAGE_FILES.includes(ext)) {
-    console.log({ ext });
-    return cb(new Error("Only images are allowed"), false);
-  }
-
-  return cb(null, true);
-};
-const storage = multer.diskStorage({
-  destination: (req, file, cb) => {
-    cb(null, path.join(__dirname, filePath));
-  },
-  filename: (req, file, cb) => {
-    const { fieldname } = file;
-    const uniqueName = Date.now() + "-" + Math.round(Math.random() * 1e9);
-    const originName = file.originalname.toLowerCase().split(" ").join("-");
-    const name = `${fieldname}-${uniqueName}-${originName}`;
-
-    cb(null, name);
-  },
-});
-const upload = multer({ storage: storage, fileFilter: fileFilter });
+const upload = uploadConfig();
 
 router.post(routes.index, upload.single("avatar"), (req: Request, res: Response<ResponseAPI>) => {
   // console.log(req.file); // -> req.file is the `avatar` file

14585d6b308bbe2bc2288635109c45b76830e81b
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2022-03-02 19:33:28 +0700

    Add API constant for DB connection

--- express-practices/express-ts/src/constants/api.ts
+++ express-practices/express-ts/src/constants/api.ts
@@ -1,3 +1,7 @@
 import "dotenv/config";
 
 export const JWT_SECRET = process.env.JWT_SECRET || "";
+export const DB_HOST = process.env.DB_HOST || "";
+export const DB_USERNAME = process.env.DB_USERNAME || "";
+export const DB_PASSWORD = process.env.DB_PASSWORD || "";
+export const DB_DBNAME = process.env.DB_DBNAME || "";

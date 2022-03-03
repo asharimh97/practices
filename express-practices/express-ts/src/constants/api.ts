d989bf2d0a0e0a55be30c5ad4aecbeb74a1127d8
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2022-03-03 17:38:21 +0700

    Add refresh token API

--- express-practices/express-ts/src/constants/api.ts
+++ express-practices/express-ts/src/constants/api.ts
@@ -1,6 +1,7 @@
 import "dotenv/config";
 
 export const JWT_SECRET = process.env.JWT_SECRET || "";
+export const JWT_REFRESH_SECRET = process.env.JWT_REFRESH_SECRET || "";
 export const DB_HOST = process.env.DB_HOST || "";
 export const DB_USERNAME = process.env.DB_USERNAME || "";
 export const DB_PASSWORD = process.env.DB_PASSWORD || "";

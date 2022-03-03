371ffde3bbe2ba165ed07a4169f6f2c9e0f0bcc7
Ashari Muhammad Hisbulloh <asharimh97@gmail.com>
2022-03-03 17:38:31 +0700

    Add helper to generate refresh token

--- express-practices/express-ts/src/shared/auth.ts
+++ express-practices/express-ts/src/shared/auth.ts
@@ -1,5 +1,6 @@
+/* eslint-disable @typescript-eslint/no-unsafe-return */
 /* eslint-disable @typescript-eslint/no-unsafe-argument */
-import { JWT_SECRET } from "@constants/api";
+import { JWT_REFRESH_SECRET, JWT_SECRET } from "@constants/api";
 import { IUser } from "@models/user-model";
 import { Request, Response, NextFunction } from "express";
 import StatusCodes from "http-status-codes";
@@ -9,11 +10,30 @@ import expressJwt from "express-jwt";
 const { UNAUTHORIZED } = StatusCodes;
 
 const tokenSecret = JWT_SECRET;
+const refreshSecret = JWT_REFRESH_SECRET;
+
 export const generateAccessToken = (user: IUser) => {
   return jwt.sign(user, tokenSecret, { expiresIn: "30s", algorithm: "HS256" });
 };
 
+export const generateRefreshToken = (user: IUser) => {
+  return jwt.sign(user, refreshSecret, { expiresIn: "30d", algorithm: "HS256" });
+};
+
 export const verifyAccessToken = expressJwt({ secret: tokenSecret, algorithms: ["HS256"] });
+export const verifyRefreshToken = expressJwt({
+  secret: refreshSecret,
+  algorithms: ["HS256"],
+  getToken: (req: Request) => {
+    const { token } = req.body;
+
+    if (!token) {
+      return null;
+    }
+
+    return token;
+  },
+});
 
 // verify jwt token, if valid, next()
 // otherwise return error
